8a2880cee995f50e9da07aa0307c20ad8162ad0a
metesahankurt <weetzstudios@icloud.com>
2025-06-09 01:08:23 +0300

    DbConfig Dosyasi Duzenlenmesi Yapildi.

--- DbConfig.cs
+++ DbConfig.cs
@@ -3,6 +3,6 @@ namespace FaturaKasaSistemi
     public static class DbConfig
     {
         // Veritabanı bağlantı bilgilerinizi buradan güncelleyebilirsiniz.
-        public static string ConnectionString = "server=localhost;database=faturakasa;user=root;password=199419033Mm/;";
+        public static string ConnectionString = "server=localhost;database=faturakasa;user=root;password=sizinSifreniz;";
     }
 } 
